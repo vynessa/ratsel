79c11a4b580f1f2c22e8c08d84458026f50a51d0
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-12-03 15:21:42 +0100

    Refactor encryption class

--- lib/ratsel/encrypt.rb
+++ lib/ratsel/encrypt.rb
@@ -1,29 +1,33 @@
 require_relative "helpers"
 
 class Encrypt
-  # Cipher array
-  def character_map 
-    ['a', 'b', 'c', 'd', 'e',
-    'f', 'g', 'h', 'i', 'j', 'k',
-    'l', 'm', 'n', 'o', 'p', 'q',
-    'r', 's', 't', 'u', 'v', 'w',
-    'x', 'y', 'z', 1, 2, 3, 4, 5,
-    6, 7, 8, 9, ' ', '.', ',']
-  end
+  def encrypt(message_txt, encrypted_txt)
+    encrypted_message = []
+    sliced_message_array = []
+    @character_map = Helpers.character_map  
+    @message = Helpers.read_file_text(message_txt).split('')
 
-  def encrypt
-    encrypted_message = []  
-    @character_map = character_map
-    @sum_rotation_offset = Helpers.sum_rotation_offset
-    message_array = Helpers.read_file_text('/test.txt').downcase.split('')
-    n = message_array.length
-  
-    n.times { 
-      char_rotate = @character_map.rotate(@character_map.index(message_array.shift))
-      rotated_array = char_rotate.rotate(@sum_rotation_offset.shift)
-      encrypted_message << rotated_array.shift
+    @message.each_slice(4) { |message|
+      sliced_message_array << message.join('')
     }
+
+    sliced_message_array.each_with_index { |batch, index|
+      @sum_rotation_offset = Helpers.sum_rotation_offset(encrypted_txt)
+      message_array = batch.downcase.split('')
+      
+      n = message_array.length
   
-    encrypted_message.join
+      n.times { 
+        char_rotate = @character_map.rotate(@character_map.index(message_array.shift))
+        rotated_array = char_rotate.rotate(@sum_rotation_offset.shift)
+        encrypted_message << rotated_array.shift
+      }
+    }
+      
+    encrypted_message = encrypted_message.join
+
+    File.open(encrypted_txt, "a") do |line|
+      line.write("\n#{encrypted_message}")   
+    end
   end
 end
--- lib/ratsel/helpers.rb
+++ lib/ratsel/helpers.rb
@@ -1,19 +1,41 @@
 class Helpers
-  # Read text from file
-  def self.read_file_text(text_file)
-    message = File.open(File.dirname(__FILE__) + text_file).each { |line| return line }
+  # Encryted info
+  def self.write_encrypted_texts(text_file)
+    encrypted_object = []
+    File.open(File.dirname(__FILE__) + text_file).each { |line|
+      encrypted_object << line
+    }
+
+    encrypted_object
   end
 
   # Get date in DDMMYY format
-  def self.get_date_time
+  def self.get_date(encrypt_txt)
     date = Time.new
     date = date.day.to_s + date.month.to_s  + (date.year % 100).to_s
+
+    File.open(encrypt_txt, "w") do |f|     
+      f.write("#{date}")   
+    end
+
+    date
   end  
-  
+
+  # Generate a random 5 digits key
+  def self.generated_key(encrypt_txt)
+    key = (0...5).map { (1..9).to_a[rand(9)] }.join
+
+    File.open(encrypt_txt, "a") do |f|     
+      f.write("\n#{key}")   
+    end
+
+    key
+  end
+
   # Get offsets array from the date of message transmission
-  def self.offsets_array
+  def self.offsets_array(encrypt_txt)
     offsets_array = []
-    date_square = get_date_time.to_i ** 2
+    date_square = get_date(encrypt_txt).to_i ** 2
     new_str = date_square.to_s.reverse
     new_str = new_str.slice!(0..3).reverse.split('')
 
@@ -22,28 +44,41 @@ class Helpers
     offsets_array
   end
 
-  # Generate a random 5 digits key  
-  def self.rotation_array
-    key_rotor_array = []   
-    key = (0...5).map { (1..9).to_a[rand(9)] }.join
-    key_array = key.split('')
-    n = key_array.length - 1 
+  # Get offsets from generated key
+  def self.rotation_array(encrypt_txt)
+    key_rotor_array = [] 
+    key_array = generated_key(encrypt_txt).split('')
       
-    n.times { key_rotor_array << (key_array.shift + key_array[0]).to_i }
+    4.times { key_rotor_array << (key_array.shift + key_array[0]).to_i }
 
     key_rotor_array
   end
 
   # Sum of A - n rotation and A - n offsets
-  def self.sum_rotation_offset
+  def self.sum_rotation_offset(encrypt_txt)
     sum_array = []
-    @offsets_array = offsets_array
-    @rotation_array = rotation_array
-    n = @offsets_array.length || @rotation_array.length
+    @offsets_array = offsets_array(encrypt_txt)
+    @rotation_array = rotation_array(encrypt_txt)
 
-    n.times { sum_array << @offsets_array.shift + @rotation_array.shift }
+    4.times { sum_array << @offsets_array.shift + @rotation_array.shift }
 
     sum_array
+  end
+
+  # Read text from file
+  def self.read_file_text(text_file)
+    message = ''
+    File.open(File.dirname(__FILE__) + text_file, 'rb').each { |line| message += line }
+    new_message = message.downcase
+  end
+
+  def self.character_map 
+    ['a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j', 'k',
+    'l', 'm', 'n', 'o', 'p', 'q',
+    'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z', '1', '2', '3', '4', '5',
+    '6', '7', '8', '9', ' ', '.', ',']
   end
 end
 
