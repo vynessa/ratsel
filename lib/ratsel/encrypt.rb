7d8d5676f97a411c048d0b45fe0f2f809e3bfccd
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2018-01-05 15:48:03 +0100

    Refactor encrypt, decrypt class and helper methods

--- lib/ratsel/encrypt.rb
+++ lib/ratsel/encrypt.rb
@@ -1,5 +1,4 @@
 require_relative "helpers/accessor"
-require_relative "helpers/cipher"
 require_relative "helpers/helpers"
 require_relative "helpers/message"
 
@@ -13,7 +12,6 @@ module Ratsel
       @message_txt = message_txt
       @encrypted_txt = encrypted_txt
       @encryption_date = Time.now.strftime("%d%m%y")
-      @character_map = Cipher.character_map
       @encryption_key = (1..5).map { (1..9).to_a[rand(9)] }.join
       @offsets_array = Helpers.offsets_array(@encryption_date)
       @rotation_array = Helpers.rotation_array(@encryption_key)
@@ -22,26 +20,11 @@ module Ratsel
 
     def encrypt
       begin
-        message = Accessor.read_file_text(@message_txt)
-        encrypted_message = ''
-        sliced_message_array = []
+        @message = Accessor.read_file_text(@message_txt)
 
-        message.each_slice(4) { |message|
-          sliced_message_array << message.join('')
-        }
+        encrypted_message = Ratsel::Batch.get_batch(@message, @sum_rotation_offset)
 
-        sliced_message_array.each_with_index { |batch, index|
-          message_batch = batch.downcase.split('')
-          n = message_batch.length
-      
-          n.times { |i|
-            char_rotate = @character_map.rotate(@character_map.index(message_batch.shift))
-            rotated_array = char_rotate.rotate(@sum_rotation_offset[i])
-            encrypted_message << rotated_array.shift
-          }
-        }
-
-        message(encrypted_txt, encryption_key, encryption_date)
+        log_message(encrypted_txt, encryption_key, encryption_date)
 
         Accessor.write_encrypted_texts(encrypted_txt, encrypted_message)
     
--- lib/ratsel/helpers/message.rb
+++ lib/ratsel/helpers/message.rb
@@ -1,6 +1,6 @@
 module Ratsel
   module SendMessage
-    def message(filename, key, date)
+    def log_message(filename, key, date)
       puts "created #{filename} with key #{key} and date #{date}"
     end
   end
